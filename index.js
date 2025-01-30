dee7b2d94e6da03229c6ab4c90295be171eb4e89
Santosh Pawar <santoshjpawar@gmail.com>
2025-01-30 16:17:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const core = require('@actions/core');
 async function run() {
     const person = core.getInput('person');
     core.info('Hello World! ' + person);
+    core.info('Env ' + process.env.VAR)
 }
 
 run();
